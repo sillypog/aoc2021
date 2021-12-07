82e42e6ed5584ef6729a3900fb7ef8760fd87207
Peter Hastie <phastie@bleacherreport.com>
2021-12-06 18:26:43 -0800

    Finally finished day 5 part 1.
    I was over counting because I was counting spaces again the next time
    another line overlapped.

--- 05a.cpp
+++ 05a.cpp
@@ -2,7 +2,8 @@
 #include <fstream>
 #include <string>
 #include <sstream>
-#include <unordered_set>
+#include <unordered_map>
+#include <vector>
 
 using namespace std;
 
@@ -11,9 +12,35 @@ struct Point {
 	int y;
 };
 
-struct Line {
+class Line {
+private:
+	Point orderAscending(int a, int b) {
+		return a < b ? Point {a, b} : Point {b, a};
+	}
+
+public:
 	Point start;
 	Point end;
+
+	vector<Point> getPoints() {
+		vector<Point> points;
+
+		if (start.x == end.x) { // Vertical line
+			Point limits = orderAscending(start.y, end.y);
+
+			for (int i = limits.x; i <= limits.y; i++) {
+				points.push_back({start.x, i});
+			}
+		} else if (start.y == end.y) { // Horizontal line
+			Point limits = orderAscending(start.x, end.x);
+
+			for (int i = limits.x; i <= limits.y; i++) {
+				points.push_back({i, start.y});
+			}
+		}
+
+		return points;
+	}
 };
 
 Line parseLine(string input) {
@@ -37,29 +64,45 @@ Line parseLine(string input) {
 	return {{startX, startY}, {endX, endY}};
 }
 
-// Do I want to store the map as a 2D array or a map keyed on marked squares?
-int markLine(Line line, unordered_set<string>& map) {
-	int overlaps = 0;
-	if (line.start.x == line.end.x || line.start.y == line.end.y) { // Might need one check for horizontal, one for vert
-		// Loop for the length of the line. NOTE: line could be decreasing OR increasing.
-		// Build string from coordinates
-		// Check against the map
-		//   If it's not there, add it
-		//   If it is there, incremement the return value
+void markLine(Line line, unordered_map<string, int>& map) {
+	stringstream keystream;
+	string key;
+
+	vector<Point> points = line.getPoints();
+	for (const Point& point : points) {
+		keystream.str("");
+		keystream << point.x << "," << point.y;
+		key = keystream.str();
+		cout << "  Checking point " << key << endl;
+
+		auto it = map.find(key);
+		if (it != map.end()) {
+			cout << "    " << key << " was in the map already" << endl;
+			it->second++;
+		} else {
+			map.insert({key, 0});
+		}
 	}
-	return overlaps;
 }
 
 int main() {
-	fstream file("05small.txt", ios::in);
-	unordered_set<string> seenPoints;
-	int multipleOverlaps = 0;
+	fstream file("05.txt", ios::in);
+	unordered_map<string, int> seenPoints;
 
 	string inputLine;
 	while (getline(file, inputLine)) {
 		Line line = parseLine(inputLine);
-		cout << line.start.x << "," << line.start.y << " to " << line.end.x << "," << line.end.y << endl;
-		int overlaps = markLine(line, seenPoints);
-		multipleOverlaps += overlaps;
+		cout << "Doing line " << line.start.x << "," << line.start.y << " to " << line.end.x << "," << line.end.y << endl;
+		markLine(line, seenPoints);
 	}
+
+	int multipleOverlaps = 0;
+	for (const auto& pair : seenPoints) {
+		cout << pair.first << ": " << pair.second << endl;
+		if (pair.second > 0) {
+			multipleOverlaps++;
+		}
+	}
+
+	cout << "There were " << multipleOverlaps << " points where two or more lines overlapped." << endl;
 }
