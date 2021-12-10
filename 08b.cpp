5ddcabbb0ba1cff3683bac435798a8aac5357505
Peter Hastie <pete@sillypog.com>
2021-12-09 19:59:41 -0800

    Day 8 cleaned up.

--- 08b.cpp
+++ 08b.cpp
@@ -1,3 +1,53 @@
+/*
+Through a little deduction, you should now be able to determine the remaining digits. Consider again the first example above:
+
+acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab |
+cdfeb fcadb cdfeb cdbaf
+After some careful analysis, the mapping between signal wires and segments only make sense in the following configuration:
+
+ dddd
+e    a
+e    a
+ ffff
+g    b
+g    b
+ cccc
+So, the unique signal patterns would correspond to the following digits:
+
+acedgfb: 8
+cdfbe: 5
+gcdfa: 2
+fbcad: 3
+dab: 7
+cefabd: 9
+cdfgeb: 6
+eafb: 4
+cagedb: 0
+ab: 1
+Then, the four digits of the output value can be decoded:
+
+cdfeb: 5
+fcadb: 3
+cdfeb: 5
+cdbaf: 3
+Therefore, the output value for this entry is 5353.
+
+Following this same process for each entry in the second, larger example above, the output value of each entry can be determined:
+
+fdgacbe cefdb cefbgd gcbe: 8394
+fcgedb cgb dgebacf gc: 9781
+cg cg fdcagb cbg: 1197
+efabcd cedba gadfec cb: 9361
+gecf egdcabf bgf bfgea: 4873
+gebdcfa ecba ca fadegcb: 8418
+cefg dcbef fcge gbcadfe: 4548
+ed bcgafe cdgba cbgef: 1625
+gbdfcae bgc cg cgb: 8717
+fgae cfgab fg bagce: 4315
+Adding all of the output values in this larger example produces 61229.
+
+For each entry, determine all of the wire/segment connections and decode the four-digit output values. What do you get if you add up all of the output values?
+ */
 #include <algorithm>
 #include <fstream>
 #include <iostream>
@@ -9,37 +59,35 @@
 
 using namespace std;
 
-constexpr int ONE_SEGMENTS = 2;
-constexpr int FOUR_SEGMENTS = 4;
-constexpr int SEVEN_SEGMENTS = 3;
-constexpr int EIGHT_SEGMENTS = 7;
+constexpr int NUM_SEGMENTS = 7;
 
 constexpr int sortedDigitPosition[10] = {6, 0, 3, 3, 2, 3, 6, 1, 9 ,6};
 
+vector<set<int>> segmentsInDigits; // The reference to which segment draws a given digit
+
+void populateSegmentsInDigits() {
+	vector<set<int>> temp {
+		{0, 1, 2, 3, 4, 5},
+		{1, 2},
+		{0, 1, 6, 4, 3},
+		{0, 1, 6, 2, 3},
+		{5, 6, 1, 2},
+		{0, 5, 6, 2, 3},
+		{6, 2, 3, 4, 5, 0},
+		{0, 1, 2},
+		{0, 1, 2, 3, 4, 5, 6},
+		{0, 1, 2, 3, 5, 6}
+	};
+
+	segmentsInDigits = temp;
+}
+
 class Display {
 private:
-	vector<string> patterns;
-	vector<string> outputs;
+	vector<string> patterns; // These are the set of strings representing all 10 digits, read from the file.
+	vector<string> outputs; // These are the set of strings representing the value of this display, read from the file.
 	vector<char> segments; // Top bar is 0, then go round clockwise. Middle bar is 6.
-	vector<set<int>> segmentsInDigits; // The reference to which segment draws a given digit
 	unordered_map<string, int> signalsForDigits;
-
-	void populateSegmentsInDigits() {
-		vector<set<int>> temp {
-			{0, 1, 2, 3, 4, 5},
-			{1, 2},
-			{0, 1, 6, 4, 3},
-			{0, 1, 6, 2, 3},
-			{5, 6, 1, 2},
-			{0, 5, 6, 2, 3},
-			{6, 2, 3, 4, 5, 0},
-			{0, 1, 2},
-			{0, 1, 2, 3, 4, 5, 6},
-			{0, 1, 2, 3, 5, 6}
-		};
-
-		segmentsInDigits = temp;
-	}
 
 	set<char> extractSignalsFromPattern(string pattern) {
 		set<char> output;
@@ -53,7 +101,7 @@ private:
 	// For each of the segments in everything else, remove this set from its possiblities
 	void eliminatePossibleSegments(int digit, set<char> digitSignals, vector<set<char>>& segmentPossibilities) {
 		set<char> temp;
-		for (int i = 0; i < EIGHT_SEGMENTS; i++) {
+		for (int i = 0; i < NUM_SEGMENTS; i++) {
 			if (segmentsInDigits[digit].count(i)) {
 				set_intersection(segmentPossibilities[i].begin(), segmentPossibilities[i].end(), digitSignals.begin(), digitSignals.end(), std::inserter(temp, temp.begin()));
 			} else {
@@ -64,25 +112,15 @@ private:
 		}
 	}
 
-	void printSegmentPossibilities(const vector<set<char>>& segmentPossibilities) {
-		// Verify that worked
-		for (int i = 0; i < EIGHT_SEGMENTS; i++) {
-			cout << segmentPossibilities[i].count('a') << segmentPossibilities[i].count('b') << segmentPossibilities[i].count('c') << segmentPossibilities[i].count('d') << segmentPossibilities[i].count('e') << segmentPossibilities[i].count('f') << segmentPossibilities[i].count('g') << endl;
-		}
-		cout << endl;
-	}
-
-	string setContents(const set<char>& s) {
-		stringstream ss;
-		for (const char c : s) {
-			ss << c;
-		}
-		return ss.str();
+	bool identifySixSegmentDigit(const set<char>& digitSignals, const set<char>& segmentPossibilities) {
+		set<char> intersection;
+		set_intersection(digitSignals.begin(), digitSignals.end(), segmentPossibilities.begin(), segmentPossibilities.end(), std::inserter(intersection, intersection.begin()));
+		return intersection.size() == 1;
 	}
 
 	void assignSegments() {
 		// Store the possibilities for each segment as we go
-		vector<set<char>> segmentPossibilities(EIGHT_SEGMENTS, {'a', 'b', 'c', 'd', 'e', 'f', 'g'});
+		vector<set<char>> segmentPossibilities(NUM_SEGMENTS, {'a', 'b', 'c', 'd', 'e', 'f', 'g'});
 
 		// Sort the patterns by length so that we can identify 1, 7, etc
 		sort(patterns.begin(), patterns.end(), [](string s1, string s2) {
@@ -91,27 +129,20 @@ private:
 
 		// Pattern for 1 is in position 0, pattern for 7 is in position 1.
 		// The additional character in position 1 is the signal for the top segment
-		/* cout << "\nDecoding digit 1: " << patterns[sortedDigitPosition[1]] << endl; */
 		set<char> digitSignals = extractSignalsFromPattern(patterns[sortedDigitPosition[1]]);
 		eliminatePossibleSegments(1, digitSignals, segmentPossibilities);
-		/* printSegmentPossibilities(segmentPossibilities); */
 
 		// Now compare 7 to 1 to get signal for segment 0
-		/* cout << "\nDecoding digit 7: " << patterns[sortedDigitPosition[7]] << endl; */
 		digitSignals = extractSignalsFromPattern(patterns[sortedDigitPosition[7]]);
 		eliminatePossibleSegments(7, digitSignals, segmentPossibilities);
-		/* printSegmentPossibilities(segmentPossibilities); */
 
 		// Now compare 4 to 1 to reduce possibilities for segments 5 and 6
-		/* cout << "\nDecoding digit 4: " << patterns[sortedDigitPosition[4]] << endl; */
 		digitSignals = extractSignalsFromPattern(patterns[sortedDigitPosition[4]]);
 		eliminatePossibleSegments(4, digitSignals, segmentPossibilities);
-		/* printSegmentPossibilities(segmentPossibilities); */
 
 		// Now use 6 to distinguish segment 1 from segment 2
 		// Identify digit six - it is the only one of length 6 that is missing segment 1
 		int startOfSixSegmentNumbers = sortedDigitPosition[6]; // 6, 9, 0 all have six digits
-		set<char> intersection;
 		bool matched6 = false;
 		bool matched9 = false;
 		bool matched0 = false;
@@ -119,11 +150,7 @@ private:
 			digitSignals = extractSignalsFromPattern(patterns[startOfSixSegmentNumbers + i]);
 			// Only 6 doesn't have segment 1
 			if (!matched6) {
-				/* cout << "Checking if " << patterns[startOfSixSegmentNumbers + i] << " is signal for 6" << endl; */
-				intersection.clear();
-				set_intersection(digitSignals.begin(), digitSignals.end(), segmentPossibilities[1].begin(), segmentPossibilities[1].end(), std::inserter(intersection, intersection.begin()));
-				if (intersection.size() == 1) {
-					/* cout << "Digit six representation is " << patterns[startOfSixSegmentNumbers + i] << endl; */
+				if (identifySixSegmentDigit(digitSignals, segmentPossibilities[1])) {
 					eliminatePossibleSegments(6, digitSignals, segmentPossibilities);
 					matched6 = true;
 					continue;
@@ -131,12 +158,7 @@ private:
 			}
 			// Only 9 doesn't have segment 4
 			if (!matched9) {
-				/* cout << "Checking if " << patterns[startOfSixSegmentNumbers + i] << " is signal for 9" << endl; */
-				intersection.clear();
-				set_intersection(digitSignals.begin(), digitSignals.end(), segmentPossibilities[4].begin(), segmentPossibilities[4].end(), std::inserter(intersection, intersection.begin()));
-				/* cout << "digitSignals: " << setContents(digitSignals) << ", segment 4 possibilities: " << setContents(segmentPossibilities[4]) << ". Intersection: " << setContents(intersection) << endl; */
-				if (intersection.size() == 1) {
-					/* cout << "Digit nine representation is " << patterns[startOfSixSegmentNumbers + i] << endl; */
+				if (identifySixSegmentDigit(digitSignals, segmentPossibilities[4])) {
 					eliminatePossibleSegments(9, digitSignals, segmentPossibilities);
 					matched9 = true;
 					continue;
@@ -144,11 +166,7 @@ private:
 			}
 			/* // Only 0 doesn't have segment 6 */
 			if (!matched0) {
-				/* cout << "Checking if " << patterns[startOfSixSegmentNumbers + i] << " is signal for 0" << endl; */
-				intersection.clear();
-				set_intersection(digitSignals.begin(), digitSignals.end(), segmentPossibilities[0].begin(), segmentPossibilities[0].end(), std::inserter(intersection, intersection.begin()));
-				if (intersection.size() == 1) {
-					/* cout << "Digit zero representation is " << patterns[startOfSixSegmentNumbers + i] << endl; */
+				if (identifySixSegmentDigit(digitSignals, segmentPossibilities[0])) {
 					eliminatePossibleSegments(0, digitSignals, segmentPossibilities);
 					matched0 = true;
 					continue;
@@ -156,16 +174,12 @@ private:
 			}
 		}
 
-		// Verify that worked
-		/* printSegmentPossibilities(segmentPossibilities); */
-
 		// Set segments based on final segmentPossibilities
 		// I only know one way to get the value out of the set so I'll loop over the one value
-		for (int i = 0; i < EIGHT_SEGMENTS; i++) {
+		for (int i = 0; i < NUM_SEGMENTS; i++) {
 			for (char c : segmentPossibilities[i]) {
 				segments.push_back(c);
 			}
-			/* cout << "Segment " << i << " mapped to signal " << segments[i] << endl; */
 		}
 	}
 
@@ -211,28 +225,17 @@ public:
 			}
 
 			if (reachedDelimiter) {
-				/* cout << "Adding " << s << " to output" << endl; */
 				outputs.push_back(s);
 			} else {
-				/* cout << "Adding " << s << " to patterns" << endl; */
 				patterns.push_back(s);
 			}
 		}
-
-		// Build up our reference for how each digit is drawn
-		populateSegmentsInDigits(); // I shouldn't be doing this for every object.
 
 		// We have all the patterns now, figure out which segments are which
 		assignSegments();
 
 		// Now that we know which segment is which, we can map all the patterns to digits
 		mapSignalsToDigits();
-
-		// Test that
-		/* cout << "signalsForDigits contains:" << endl; */
-		/* for (auto entry : signalsForDigits) { */
-		/* 	cout << entry.first << ": " << entry.second << endl; */
-		/* } */
 	}
 
 	int sumOutputs() const {
@@ -244,10 +247,8 @@ public:
 			sum += (digit * unit);
 			unit /= 10;
 		}
-		/* cout << sum << ", "; */
 		return sum;
 	}
-
 };
 
 
@@ -256,8 +257,10 @@ int main() {
 	fstream file("08.txt", ios::in);
 	string line;
 	int sum;
-
 	vector<Display> displays;
+
+	// Build up our reference for how each digit is drawn
+	populateSegmentsInDigits();
 
 	while (getline(file, line)) {
 		displays.emplace_back(line);
@@ -267,6 +270,5 @@ int main() {
 		sum += display.sumOutputs();
 	}
 
-	cout << "There are " << displays.size() << " displays" << endl;
 	cout << "The sum of display outputs is " << sum << endl;
 }
